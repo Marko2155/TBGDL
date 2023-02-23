1c290735909aa2f3330a7e8ad0dafd89c7401989
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2023-02-23 14:10:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='libmath',
+    name='TBGDL',
     long_description_content_type='text/markdown',
     long_description=long_description,
-    keywords="math, maths",
+    keywords="pygame, textadventure, text, game",
     version='1.0.0',
-    packages=['libmath'],
-    url='https://github.com/Marko2155/libmath',
+    packages=['tbgdl'],
+    url='https://github.com/Marko2155/TBGDL',
     license='MIT',
     author='Marko Camandioti',
     author_email='camandiotimarko@gmail.com',
-    description="A Python library for math.",
+    description="A Python library for creating text-based games.",
     project_urls={
-        'Documentation': 'https://github.com/Marko2155/libmath#readme',
-        'Source': 'https://github.com/Marko2155/libmath',
-        'Tracker': 'https://github.com/Marko2155/libmath/issues',
+        'Documentation': 'https://github.com/Marko2155/TBGDL#readme',
+        'Source': 'https://github.com/Marko2155/TBGDL',
+        'Tracker': 'https://github.com/Marko2155/TBGDL/issues',
     }
 )
